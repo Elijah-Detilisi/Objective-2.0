7d2136d17056710b59842b86d395bb65697afef9
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-03-25 13:54:35 +0200

    Add data access class

---
+++ Objective.Maui_App/DataServives/.Base/DataAccess.cs
@@ -0,0 +1,45 @@
+﻿using SQLite;
+using Objective.Maui_App.Models.Base;
+
+namespace Objective.Maui_App.DataServives.Base
+{
+    public abstract class DataAccess
+    {
+        #region Fields
+        protected SQLiteAsyncConnection _connection;
+        private string _connectionString => Path.Combine(FileSystem.AppDataDirectory, "objective.db3");
+        #endregion
+
+        #region Construction
+        public DataAccess()
+        {
+            if (_connection == null)
+            {
+                _connection = new SQLiteAsyncConnection(_connectionString);
+
+               CreateRepoTable();
+            }
+        }
+
+        #endregion
+
+        #region CRUD methods
+        protected abstract void CreateRepoTable();
+        public async Task<int> Add(Model model)
+        {
+            return await _connection.InsertAsync(model);
+        }
+        public async Task<int> Update(Model model)
+        {
+            return await _connection.UpdateAsync(model);
+        }
+        public async Task<int> Delete(Model model)
+        {
+            return await _connection.DeleteAsync(model);
+        }
+        public abstract Task<List<Model>> Get(int? id);
+
+        #endregion
+
+    }
+}
