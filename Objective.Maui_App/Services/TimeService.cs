d7bb55ccc6c1520397eed9a66aafcc6870f8c6f8
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-04-01 03:43:23 +0200

    Added time service

---
+++ Objective.Maui_App/Services/TimeService.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Objective.Maui_App.Services
+{
+    public static class TimeService
+    {
+        public static string Time()
+        {
+            return DateTime.Now.ToString("t");
+        }
+        public static string Today()
+        {
+            return DateTime.Now.ToString("dd MMMM, yyyy");
+        }
+
+        public static string TimeOfDay()
+        {
+            string timeOfDay = "Morning";
+            int currentHour = DateTime.Now.Hour;
+
+            if(currentHour>12 && currentHour<18)
+            {
+                timeOfDay = "Afternoon";
+            }
+            else if (currentHour>=18 && currentHour<=23)
+            {
+                timeOfDay = "Evening";
+            }
+
+            return timeOfDay;
+        }
+    }
+}
