cefa43d7c09e4245b4edb878aed08b5ad4eeff5e
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-04-02 09:57:05 +0200

    Add bindable property on MainHeader content

--- Objective.Maui_App/Views/Templates/MainHeader.xaml.cs
+++ Objective.Maui_App/Views/Templates/MainHeader.xaml.cs
@@ -2,8 +2,21 @@ namespace Objective.Maui_App.Views.Templates;
 
 public partial class MainHeader : ContentView
 {
-	public MainHeader()
+    public readonly BindableProperty GreetingTitleProperty;
+    public string GreetingMessage
+    {
+        get => (string)GetValue(GreetingTitleProperty);
+        set => SetValue(GreetingTitleProperty, value);
+    }
+
+    public MainHeader()
 	{
-		InitializeComponent();
+        GreetingTitleProperty = BindableProperty.Create
+        (
+            nameof(GreetingMessage), 
+            typeof(string), typeof(MainHeader), string.Empty
+        );
+
+        InitializeComponent();
 	}
 }
