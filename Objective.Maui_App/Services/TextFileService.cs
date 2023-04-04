1210440feb881f8576defe59be70ad4638c8e772
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-04-04 21:36:32 +0200

    Added TextFileService to read textf iles

---
+++ Objective.Maui_App/Services/TextFileService.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Objective.Maui_App.Services
+{
+    public static class TextFileService
+    {
+        public static async Task<string> ReadFile(string textFile)
+        {
+            try
+            {
+                using Stream fileStream = await FileSystem.Current.OpenAppPackageFileAsync(textFile);
+                using StreamReader reader = new StreamReader(fileStream);
+
+                var result = await reader.ReadToEndAsync();
+
+                return result;
+            }
+            catch (Exception ex)
+            { 
+                Console.Write(ex.ToString());
+            }
+
+            return String.Empty;
+        }
+
+    }
+}
