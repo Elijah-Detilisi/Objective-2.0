8885c87cffc1f4b452e12b499e2d458b3186ee7f
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-04-04 22:52:32 +0200

    Added reading files line by line in TextFileService

--- Objective.Maui_App/Services/TextFileService.cs
+++ Objective.Maui_App/Services/TextFileService.cs
@@ -22,5 +22,30 @@ namespace Objective.Maui_App.Services
             return String.Empty;
         }
 
+        public static async Task<List<string>> ReadFileLines(string textFile)
+        {
+            try
+            {
+                string line = String.Empty;
+                var result = new List<string>();
+
+                using Stream fileStream = await FileSystem.Current.OpenAppPackageFileAsync(textFile);
+                using StreamReader reader = new StreamReader(fileStream);
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    result.Add(line);
+                }
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.ToString());
+            }
+
+            return new List<string>();
+        }
+
     }
 }
