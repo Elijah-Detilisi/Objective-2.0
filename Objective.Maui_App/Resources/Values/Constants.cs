fc249f889065179ce845f0468852a4d09e9c241b
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-04-05 21:33:33 +0200

    Update constant strings

--- Objective.Maui_App/Resources/Values/Constants.cs
+++ Objective.Maui_App/Resources/Values/Constants.cs
@@ -2,7 +2,7 @@
 {
     public static class Constants
     {
-        public static readonly string CONNECTION_STRING = "objective_data.db3";
         public static readonly string QUOTES_TEXT_FILE = "102-inspirational-qoutes.txt";
+        public static readonly string CONNECTION_STRING = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "objective_data.db3");
     }
 }
