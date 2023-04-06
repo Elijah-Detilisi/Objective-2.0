93d70af6c890597ec5d87dd0f5be0ba08f6365a1
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-04-07 00:05:45 +0200

    Made header template bindable

--- Objective.Maui_App/Views/Mobile/Main/Templates/Header.xaml.cs
+++ Objective.Maui_App/Views/Mobile/Main/Templates/Header.xaml.cs
@@ -1,9 +1,57 @@
+using System.Windows.Input;
+
 namespace Objective.Maui_App.Views.Mobile.Main.Templates;
 
 public partial class Header : ContentView
 {
 	public Header()
 	{
-		InitializeComponent();
+        UsernameText = "Username";
+        GreetingText = "Good day";
+        InitializeComponent();
 	}
+
+    #region Properties
+    
+    public static readonly BindableProperty UsernameTextProperty = BindableProperty.Create(nameof(UsernameText), typeof(string), typeof(Header));
+    public static readonly BindableProperty GreetingTextProperty = BindableProperty.Create(nameof(GreetingText), typeof(string), typeof(Header));
+    
+    public static readonly BindableProperty SearchCommandProperty = BindableProperty.Create(nameof(SearchCommand), typeof(ICommand), typeof(Header),
+        defaultBindingMode: BindingMode.TwoWay, defaultValueCreator: DefaultCommand);
+    public static readonly BindableProperty ProfileCommandProperty = BindableProperty.Create(nameof(ProfileCommand), typeof(ICommand), typeof(Header),
+        defaultBindingMode: BindingMode.TwoWay, defaultValueCreator: DefaultCommand);
+
+    #endregion
+
+    #region Fields
+    public string UsernameText
+    {
+        get => (string)GetValue(UsernameTextProperty);
+        set => SetValue(UsernameTextProperty, value);
+    }
+    public string GreetingText
+    {
+        get => (string)GetValue(GreetingTextProperty);
+        set => SetValue(GreetingTextProperty, value);
+    }
+    #endregion
+
+    #region Commands
+    public ICommand SearchCommand
+    {
+        get => (ICommand)GetValue(SearchCommandProperty);
+        set => SetValue(SearchCommandProperty, value);
+    }
+
+    public ICommand ProfileCommand
+    {
+        get => (ICommand)GetValue(ProfileCommandProperty);
+        set => SetValue(ProfileCommandProperty, value);
+    }
+
+    private static object DefaultCommand(BindableObject bindable)
+    {
+        return new Command(() => TextToSpeech.Default.SpeakAsync("Null action!"));
+    }
+    #endregion
 }
