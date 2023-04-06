a541ad56390a5a1d1cdf39b2e17bb06c83b82dba
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-04-07 00:38:21 +0200

    Update code format

--- Objective.Maui_App/Models/Objective.cs
+++ Objective.Maui_App/Models/Objective.cs
@@ -6,8 +6,11 @@ namespace Objective.Maui_App.Models
     [Table(nameof(Objective))]
     public class Objective : BaseModel
     {
+        [MaxLength(100)]
         public string Title { get; set; }
+        [MaxLength(200)]
         public string Description { get; set; }
+
         public bool IsDone { get; set; } = false;
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public DateTime DueDate { get; set; } = DateTime.Today.AddDays(1);
