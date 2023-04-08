1c56ecb94a05d710a5e18123d05a1ab29ff53ef3
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-04-08 19:41:58 +0200

    Add binding to the Objective view

--- Objective.Maui_App/Views/Mobile/Main/Templates/Objective.xaml.cs
+++ Objective.Maui_App/Views/Mobile/Main/Templates/Objective.xaml.cs
@@ -4,6 +4,26 @@ public partial class Objective : ContentView
 {
 	public Objective()
 	{
-		InitializeComponent();
+        ObjectiveText = new Models.Objective()
+        {
+            Title = "Customize profile.",
+            Description = "Change your profile picture and username.",
+            DueDate = DateTime.Now.Date,
+            IsDone = false,
+        };
+
+        InitializeComponent();
 	}
+
+    //Properties
+    public static readonly BindableProperty ObjectiveProperty = BindableProperty.Create(nameof(ObjectiveText), typeof(Models.Objective), typeof(Objective));
+
+    //Fields
+    public Models.Objective ObjectiveText
+    {
+        get => (Models.Objective)GetValue(ObjectiveProperty);
+        set => SetValue(ObjectiveProperty, value);
+    }
+
+    public string DueDateText => ObjectiveText.DueDate.ToString("dd MMM yyyy");
 }
