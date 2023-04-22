2a0048fc0530f697fa33b48b9fbb38381d2e3ac5
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-04-22 18:31:51 +0200

    Added collectionview to search

--- Objective.Maui_App/Views/Mobile/Main/SearchView.xaml.cs
+++ Objective.Maui_App/Views/Mobile/Main/SearchView.xaml.cs
@@ -1,9 +1,32 @@
+using System.Collections.ObjectModel;
+
 namespace Objective.Maui_App.Views.Mobile.Main;
 
 public partial class SearchView : ContentPage
 {
-	public SearchView()
+    public ObservableCollection<Models.Objective> _objectives;
+
+    public SearchView()
 	{
-		InitializeComponent();
+        Objectives = new ObservableCollection<Models.Objective>()
+        {
+            new Models.Objective {Title = "Customize profile."},
+            new Models.Objective {Title = "Customize profile."},
+            new Models.Objective {Title = "Customize profile."},
+            new Models.Objective {Title = "Customize profile."},
+        };
+
+
+        InitializeComponent();
 	}
+
+    public ObservableCollection<Models.Objective> Objectives
+    {
+        get { return _objectives; }
+        set
+        {
+            _objectives = value;
+            OnPropertyChanged();
+        }
+    }
 }
