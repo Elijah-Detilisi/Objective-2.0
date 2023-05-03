12237fbbe0ef90a1eb76146e3966a3a8ec94f9f2
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-05-03 21:28:04 +0200

    Create TextFileService.cs

---
+++ ObjectiveApp/Services/TextFileService.cs
@@ -0,0 +1,29 @@
+﻿namespace ObjectiveApp.Services
+{
+    public class TextFileService
+    {
+        public static async Task<string> ReadFile(string textFile)
+        {
+            using Stream fileStream = await FileSystem.Current.OpenAppPackageFileAsync(textFile);
+            using StreamReader reader = new StreamReader(fileStream);
+
+            return await reader.ReadToEndAsync();
+        }
+
+        public static async Task<List<string>> ReadFileLines(string textFile)
+        {
+            string line = String.Empty;
+            var result = new List<string>();
+
+            using Stream fileStream = await FileSystem.Current.OpenAppPackageFileAsync(textFile);
+            using StreamReader reader = new StreamReader(fileStream);
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                result.Add(line);
+            }
+
+            return result;
+        }
+    }
+}
