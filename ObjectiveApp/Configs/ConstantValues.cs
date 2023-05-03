ef58a59ebd37928b0168e9b59623899bf21e1fc3
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-05-03 22:42:54 +0200

    Create ConstantValues.cs

---
+++ ObjectiveApp/Configs/ConstantValues.cs
@@ -0,0 +1,19 @@
+﻿using static System.Environment;
+
+namespace ObjectiveApp.Configs
+{
+    public static class ConstantValues
+    {
+        public static string QUOTES_TEXT_FILE
+        {
+            get { return "102-inspirational-qoutes.txt"; }
+        }
+
+        public static string SQLite_CONNECTION_STRING
+        {
+            get {
+                return Path.Combine(GetFolderPath(SpecialFolder.Personal), "objective_data.db3");
+            }
+        }
+    }
+}
