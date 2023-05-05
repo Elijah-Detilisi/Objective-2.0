56461137a97cc52e76e16538dab1aa3e4880ab4f
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-05-05 21:00:27 +0200

    Set binding for NavBarTemplate

--- ObjectiveApp/ViewModels/HomeViewModel.cs
+++ ObjectiveApp/ViewModels/HomeViewModel.cs
@@ -1,4 +1,5 @@
-﻿using ObjectiveApp.Views.Profile;
+﻿using ObjectiveApp.Services;
+using ObjectiveApp.Views.Profile;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
 
@@ -6,11 +7,32 @@ namespace ObjectiveApp.ViewModels
 {
     public partial class HomeViewModel : ObservableObject
     {
+        
+        public HomeViewModel()
+        {
+            LoadGreeting();
+        }
 
+        //Properties
+        [ObservableProperty]
+        public string greeting;
+        [ObservableProperty]
+        public string dayOfWeek;
+        [ObservableProperty]
+        public string currentUser = "User";
+
+        //Commands
         [RelayCommand]
         public async Task NavigateToNewPage()
         {
             await Shell.Current.GoToAsync(nameof(ProfileView));
+        }
+
+        //Methods
+        private void LoadGreeting()
+        {
+            Greeting = $"Good {DateTimeService.TimeOfDay()}";
+            DayOfWeek = $"Good {DateTimeService.TimeOfDay()}";
         }
     }
 }
--- ObjectiveApp/Views/Home/Templates/NavBarTemplate.xaml.cs
+++ ObjectiveApp/Views/Home/Templates/NavBarTemplate.xaml.cs
@@ -6,4 +6,23 @@ public partial class NavBarTemplate : ContentView
 	{
 		InitializeComponent();
 	}
+
+    public static readonly BindableProperty GreetingProperty = BindableProperty.Create(
+        nameof(Greeting), typeof(string), typeof(NavBarTemplate)
+    );
+    public static readonly BindableProperty CurrentUserProperty = BindableProperty.Create(
+        nameof(CurrentUser), typeof(string), typeof(NavBarTemplate)
+    );
+
+    public string Greeting
+    {
+        get => (string)GetValue(GreetingProperty);
+        set => SetValue(GreetingProperty, value);
+    }
+    public string CurrentUser
+    {
+        get => (string)GetValue(CurrentUserProperty);
+        set => SetValue(CurrentUserProperty, value);
+    }
+    
 }
