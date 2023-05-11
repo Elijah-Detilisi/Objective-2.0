d1d237ea6d2fab72487da713172b0833a0f412c8
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-05-11 20:02:55 +0200

    Added DatetimePickter template

---
+++ ObjectiveApp/Views/Objective/Templates/DatetimePickerTemplate.xaml.cs
@@ -0,0 +1,9 @@
+namespace ObjectiveApp.Views.Objective.Templates;
+
+public partial class DatetimePickerTemplate : ContentView
+{
+	public DatetimePickerTemplate()
+	{
+		InitializeComponent();
+	}
+}
