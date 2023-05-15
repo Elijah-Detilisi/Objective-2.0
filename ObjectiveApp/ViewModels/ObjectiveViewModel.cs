fde399a95cdbfba7bfee4f280daf34fd17368d2e
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-05-15 20:49:43 +0200

    Remove encapsulation from datetime picker template to allow for data binding

--- ObjectiveApp/ViewModels/ObjectiveViewModel.cs
+++ ObjectiveApp/ViewModels/ObjectiveViewModel.cs
@@ -1,6 +1,6 @@
 ﻿using ObjectiveApp.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
-using System;
+using CommunityToolkit.Mvvm.Input;
 
 namespace ObjectiveApp.ViewModels
 {
@@ -8,9 +8,30 @@ namespace ObjectiveApp.ViewModels
     {
         //Properties
         [ObservableProperty]
+        public DateTime selectedDate;
+        [ObservableProperty]
+        public TimeSpan selectedTime;
+        [ObservableProperty]
         public Objective newObjective = new()
         {
             DueDate = new DateTime(2008, 9, 23, 10, 30, 50),
         };
+
+        //Constrution
+        public ObjectiveViewModel()
+        {
+            selectedDate = NewObjective.DueDate;
+            selectedTime = TimeOnly.FromDateTime(selectedDate).ToTimeSpan();
+        }
+
+
+
+        //Commands
+        [RelayCommand]
+        public void OnSave()
+        {
+            var result1 = SelectedDate.Month;
+            var result2 = SelectedTime.ToString();
+        }
     }
 }
