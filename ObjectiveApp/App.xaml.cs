d7dc54d16134b7a1296af04b825f6790684c910a
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-06-03 23:37:35 +0200

    Update App.xaml.cs

--- ObjectiveApp/App.xaml.cs
+++ ObjectiveApp/App.xaml.cs
@@ -8,26 +8,8 @@ namespace ObjectiveApp
         public App()
         {
             InitializeComponent();
-            //RegisterCustomEntryHandler();
-
+            
             MainPage = new AppShell();
-        }
-
-        public void RegisterCustomEntryHandler()
-        {
-            Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(CustomEntry), (handler, view) =>
-            {
-                if (view is CustomEntry)
-                {
-                #if __ANDROID__
-                    handler.PlatformView.SetBackgroundColor(Colors.Transparent.ToPlatform());
-                #elif __IOS__
-                    handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
-                #elif WINDOWS
-                    handler.PlatformView.FontWeight = Microsoft.UI.Text.FontWeights.Thin;
-                #endif
-                }
-            });
         }
     }
 }
