41fb610365c1df16a02afd91cff519f61f648907
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-06-09 21:37:54 +0200

    Finalized Search view and search functionality

--- ObjectiveApp/Views/Home/_subViews/SearchSubView.xaml.cs
+++ ObjectiveApp/Views/Home/_subViews/SearchSubView.xaml.cs
@@ -1,9 +1,51 @@
+using ObjectiveApp.ViewModels;
+
 namespace ObjectiveApp.Views.Home._subViews;
 
 public partial class SearchSubView : ContentPage
 {
-	public SearchSubView()
-	{
-		InitializeComponent();
-	}
+    #region Fields
+    private HomeViewModel _viewModel;
+    #endregion
+
+    #region Construction
+    public SearchSubView(HomeViewModel viewModel)
+    {
+        _viewModel = viewModel;
+        BindingContext = viewModel;
+
+        InitializeComponent();
+    }
+    #endregion
+
+    #region App lifecycle method
+    protected override async void OnAppearing()
+    {
+        await _viewModel.LoadViewModel();
+        MyCollectionView.ItemsSource = _viewModel.ObjectiveList;
+
+        base.OnAppearing();
+    }
+    #endregion
+
+    #region Commands and Handlers
+    private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        FilterSearchListCommand(e.NewTextValue);
+    }
+    private void FilterSearchListCommand(string searchText)
+    {
+        if (string.IsNullOrWhiteSpace(searchText))
+        {
+            MyCollectionView.ItemsSource = _viewModel.ObjectiveList;
+        }
+        else
+        {
+            MyCollectionView.ItemsSource = _viewModel.ObjectiveList.Where(
+                item => item.Title.ToLower().Contains(searchText.ToLower())
+            );
+        }
+    }
+    #endregion
+
 }
