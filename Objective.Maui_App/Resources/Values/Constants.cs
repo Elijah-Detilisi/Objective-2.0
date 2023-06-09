c56ffd3ca4f58cc49bb831452fcecc260426d4be
Bongani Elijah Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-06-09 21:55:47 +0200

    Delete Objective.Maui_App directory

--- Objective.Maui_App/Resources/Values/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Objective.Maui_App.Resources.Values
-{
-    public static class Constants
-    {
-        public static readonly string QUOTES_TEXT_FILE = "102-inspirational-qoutes.txt";
-        public static readonly string CONNECTION_STRING = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "objective_data.db3");
-    }
-}
--- Objective.Maui_App/Views/Mobile/Main/Templates/Objective.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace Objective.Maui_App.Views.Mobile.Main.Templates;
-
-public partial class Objective : ContentView
-{
-	public Objective()
-	{
-        InitializeComponent();
-	}
-}
