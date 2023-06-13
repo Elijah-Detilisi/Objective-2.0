00b0c3c4042b4e1798c92ef2c2649b6bd6c93af3
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-06-13 22:33:53 +0200

    Update TextFileServiceTests.cs

--- ObjectiveApp.Tests/Services.Tests/TextFileServiceTests.cs
+++ ObjectiveApp.Tests/Services.Tests/TextFileServiceTests.cs
@@ -1,13 +1,34 @@
-﻿using Xunit;
+﻿using ObjectiveApp.Services;
+using Xunit;
 
 namespace ObjectiveApp.Tests.Services.Tests
 {
     public class TextFileServiceTests
     {
         [Fact]
-        public void SampleTest()
+        public void ReadFile_InputIs_ValidFileName_ReturnTextString()
         {
+            //Arrange
+            var validTextFile = "real_text_file.txt";
 
+            //Act
+            var result = TextFileService.ReadFile(validTextFile);   
+
+            //Assert
+            Assert.IsType<string>(result);
+        }
+
+        [Fact]
+        public void ReadFile_InputIsNot_ValidFileName_ThrowException()
+        {
+            //Arrange
+            var validTextFile = "fake_text_file.txt";
+
+            //Act
+            var result = TextFileService.ReadFile(validTextFile);
+
+            //Assert
+            Assert.ThrowsAny<ArgumentException>(() => result);
         }
     }
 }
