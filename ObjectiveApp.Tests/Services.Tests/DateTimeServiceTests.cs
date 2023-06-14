0c24eeb44ff6b3b9eb422814c8f4bd5eaf1d77a8
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-06-14 19:04:51 +0200

    Create DateTimeServiceTests.cs

---
+++ ObjectiveApp.Tests/Services.Tests/DateTimeServiceTests.cs
@@ -0,0 +1,46 @@
+﻿using Xunit;
+
+namespace ObjectiveApp.Tests.Services.Tests
+{
+    public class DateTimeServiceTests
+    {
+        [Fact]
+        public void TimeOfDay_When_Hour_Is_Before_12_Return_MorningText()
+        {
+            //Arrange
+            
+
+            //Act
+            
+
+            //Assert
+            
+        }
+
+        [Fact]
+        public void TimeOfDay_When_Hour_Is_After_12_Return_AfternoonText()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+
+        }
+
+        [Fact]
+        public void TimeOfDay_When_Hour_Is_After_18_Return_EveningText()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+
+        }
+    }
+}
