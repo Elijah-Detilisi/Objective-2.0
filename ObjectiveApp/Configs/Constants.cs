fbe294987ebd56d179bda58571d66c9f05c6f93c
Bongani Detilisi <94807345+Elijah-Detilisi@users.noreply.github.com>
2023-07-22 23:21:12 +0200

    Add data access

---
+++ ObjectiveApp/Configs/Constants.cs
@@ -0,0 +1,20 @@
+﻿namespace ObjectiveApp.Configs
+{
+    public static class Constants
+    {
+        private const string DatabaseFilename = "objectiveSQLite.db3";
+
+        public const string QuotesTextFile = "102-inspirational-qoutes.txt";
+
+        public const SQLite.SQLiteOpenFlags Flags =
+            // open the database in read/write mode
+            SQLite.SQLiteOpenFlags.ReadWrite |
+            // create the database if it doesn't exist
+            SQLite.SQLiteOpenFlags.Create |
+            // enable multi-threaded database access
+            SQLite.SQLiteOpenFlags.SharedCache;
+
+        public static string DatabasePath =>
+            Path.Combine(FileSystem.AppDataDirectory, DatabaseFilename);
+    }
+}
---
+++ ObjectiveApp/DataAccess/.Base/DataAccess.cs
@@ -0,0 +1,86 @@
+﻿using ObjectiveApp.Configs;
+using ObjectiveApp.Models.Base;
+using SQLite;
+using System.Diagnostics;
+
+namespace ObjectiveApp.DataAccess.Base
+{
+    public class DataAccess<TModel> where TModel : BaseModel, new()
+    {
+        #region Fields
+        protected SQLiteAsyncConnection _connection;
+        #endregion
+
+        #region Helper methods
+        private static void NullCheck(TModel model)
+        {
+            if (model is null)
+            {
+                throw new ArgumentNullException(nameof(model));
+            }
+        }
+        #endregion
+
+        #region Init methods
+        protected virtual async Task InitializeAsync()
+        {
+            if (_connection is null)
+            {
+                _connection = new SQLiteAsyncConnection(Constants.DatabasePath);
+                _connection.Tracer = new Action<string>(q => Debug.WriteLine(q));
+                _connection.Trace = true;
+
+                await _connection.CreateTableAsync<TModel>();
+            }
+        }
+        #endregion
+
+        #region CRUD methods
+        public async Task<int> AddAsync(TModel model)
+        {
+            DataAccess<TModel>.NullCheck(model);
+            await InitializeAsync();
+
+            return await _connection.InsertAsync(model);
+        }
+        public async Task<int> UpdateAsync(TModel model)
+        {
+            DataAccess<TModel>.NullCheck(model);
+            await InitializeAsync();
+
+            return await _connection.UpdateAsync(model);
+        }
+        public async Task SaveAsync(TModel model)
+        {
+            if (model.Id == 0)
+            {
+                await AddAsync(model);
+            }
+            else
+            {
+                await UpdateAsync(model);
+            }
+        }
+        public async Task<int> Delete(TModel model)
+        {
+            DataAccess<TModel>.NullCheck(model);
+            await InitializeAsync();
+
+            return await _connection.DeleteAsync(model);
+        }
+        public async Task<List<TModel>> GetAsync(Func<TModel, bool> predicate)
+        {
+            if (predicate is null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+
+            await InitializeAsync();
+            var items = await _connection.Table<TModel>().ToListAsync();
+
+            return items.Where(predicate).ToList();
+        }
+
+        #endregion
+    }
+}
